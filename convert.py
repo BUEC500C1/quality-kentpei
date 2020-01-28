869ee5ba19138b79c9536121feb4d48edee32d43
kentpei <48425664+kentpei@users.noreply.github.com>
2020-01-28 17:59:19 -0500

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self,num):
+        #dic = {1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',
+        #10:'X',9:'IX',5:'V',4:'IV'}
+        dic = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+                [ 100, 'C'], [ 90, 'XC'], [ 50, 'L'], [ 40, 'XL'],
+                [  10, 'X'], [  9, 'IX'], [  5, 'V'], [  4, 'IV'],
+                [   1, 'I']]
+        str = ''
+        i = 0
+        while num > 0:
+            while dic[i][0] > num:
+                i += 1
+            str += dic[i][1]
+            num -= dic[i][0]
+        return str
+'''
+if __name__ == "__main__":
+    s = Solution()
+    print(s.convert(4393))
+'''
